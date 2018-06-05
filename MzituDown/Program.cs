1e1378ff0355d27fb210d73801fe375d24a7afb7
eason <eason.ren@outlook.com>
2018-06-05 16:05:36 +0800

    add httpclient

--- MzituDown/Program.cs
+++ MzituDown/Program.cs
@@ -1,37 +1,64 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MzituDown
 {
     [Command(Description = "Mzitu Download")]
     class Program
     {
+        public const string BASE_URL = "http://www.mzitu.com/";
+        private static readonly HttpClient _httpClient;
+
+        static Program()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(BASE_URL)
+            };
+        }
 
         public static int Main(string[] args)
         {
 
 #if DEBUG
-            args = new string[] { "baidu.com", "-p", "da" };
-            Console.WriteLine("ASDSA");
-            Console.ReadKey();
+            args = new string[] { "137224", "-p", "da" };
+            //Console.WriteLine("ASDSA");
 #endif
             return CommandLineApplication.Execute<Program>(args);
-
         }
 
-        [Argument(0, Description = "Required;Mzitu url")]
+        [Argument(0, Description = "Required;Mzitu album id(e.g. http://www.mzitu.com/137224 id is 137224)")]
         [Required]
-        public string Url { get; }
+        public string Id { get; }
 
         [Option("-p|--path", Description = "Photo save path(default is the path where the current command executes)")]
         public string Path { get; } = Environment.CurrentDirectory;
 
         private int OnExecute()
         {
-            Console.WriteLine(Url);
+            Console.WriteLine(GetHtmlStringAsync(Id).Result);
             Console.WriteLine(Path);
+            Console.ReadKey();
             return 0;
+        }
+
+        public async Task<string> GetHtmlStringAsync(string path)
+        {
+            string html = string.Empty;
+            HttpResponseMessage response = await _httpClient.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                html = await response.Content.ReadAsStringAsync();
+            }
+            return html;
+        }
+
+        public void DownloadImage()
+        {
+            _httpClient.DefaultRequestHeaders.Referrer = new Uri($"{BASE_URL}{Id}");
         }
     }
 }
